d23617428770fcaa207f230bba36824247226452
rentao <rentao@aila.site>
2022-05-23 07:10:56 +0800

    配置redis

--- mongo_headlines/mongo_headlines/settings/dev.py
+++ mongo_headlines/mongo_headlines/settings/dev.py
@@ -104,6 +104,33 @@ DATABASES = {
 }
 
 
+# CACHES
+# https://django-redis-chs.readthedocs.io/zh_CN/latest/
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    "session": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    "verify_code": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
