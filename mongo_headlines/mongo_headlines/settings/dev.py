86825fdb7dcb44663cba65d2d6fbf3991a17f096
rentao <rentao@aila.site>
2022-05-23 22:10:04 +0800

    code update

--- mongo_headlines/mongo_headlines/settings/dev.py
+++ mongo_headlines/mongo_headlines/settings/dev.py
@@ -170,3 +170,49 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Logging
+# https://docs.djangoproject.com/en/2.2/topics/logging/
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,  # 是否禁用已经存在的日志器
+    'formatters': {  # 日志显示的格式
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'filters': {  # 对日志进行过滤
+        'require_debug_true': {  # django在debug模式下才输出日志
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {  # 日志处理方法
+        'console': {  # 向终端中输出
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file': {  # 向文件中输出日志
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/mongo.log'),  # 日志文件的位置
+            'maxBytes': 300 * 1024 * 1024,
+            'backupCount': 10,
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {  # 日志器
+        'django': {  # 定义了一个名字为 django 的日志器
+            'handlers': ['console', 'file'],  # 可以同时想终端和文件中输出日志
+            'propagate': True,  # 是否继续传递日志信息
+            'level': 'INFO'  # 日志器接受的最低日志级别
+        }
+    }
+}
